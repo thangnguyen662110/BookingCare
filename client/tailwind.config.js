bc5469510a7e81213162f3057051908de2d84b9c
LDAVu2k3 <leducanhvu3006@gmail.com>
2023-12-25 23:09:36 +0700

    add color config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js, ts, jsx, tsx}"],
+  content: ["./index.html", "./src/**/*.{js, ts, jsx, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#DAF2F6",
+          100: "#45C3C2",
+          200: "#34929E",
+        },
+        secondary: {
+          50: "#FFFED1",
+          100: "##FFF8D1",
+          200: "#FFF3D1",
+          300: "#FFEDD1",
+          400: "#FFE6D1",
+          500: "#FFC419",
+          600: "#FFE119",
+          700: "##FFFD19",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
